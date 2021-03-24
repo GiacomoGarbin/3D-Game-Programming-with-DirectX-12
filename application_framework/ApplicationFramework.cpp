b4202fe77203a310243337fc90a3106b3833a75c
Giacomo Garbin <giacomogarbinmailbox@gmail.com>
2021-03-24 22:05:02 +0000

    add ApplicationFramework class

---
+++ application_framework/ApplicationFramework.cpp
@@ -0,0 +1,11 @@
+#include "ApplicationFramework.h"
+
+ApplicationFramework::ApplicationFramework()
+{
+
+}
+
+ApplicationFramework::~ApplicationFramework()
+{
+    
+}
---
+++ application_framework/ApplicationFramework.h
@@ -0,0 +1,11 @@
+#ifndef APPLICATION_FRAMEWORK_H
+#define APPLICATION_FRAMEWORK_H
+
+class ApplicationFramework
+{
+public:
+    ApplicationFramework();
+    virtual ~ApplicationFramework();
+};
+
+#endif // APPLICATION_FRAMEWORK_H
