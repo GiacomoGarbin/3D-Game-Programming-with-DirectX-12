c7dda10f3e4136f8638447944c827f1ba5d3dce5
Giacomo Garbin <giacomogarbinmailbox@gmail.com>
2021-06-10 21:52:38 +0100

    update MathHelper class : add RandFloat() and RandInt() methods

--- common/MathHelper.cpp
+++ common/MathHelper.cpp
@@ -14,3 +14,18 @@ T MathHelper::clamp(const T& x, const T& a, const T& b)
 {
 	return x < a ? a : (x > b ? b : x);
 }
+
+float MathHelper::RandFloat()
+{
+    return static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+}
+
+float MathHelper::RandFloat(float a, float b)
+{
+    return a + RandFloat() * (b - a);
+}
+
+int MathHelper::RandInt(int a, int b)
+{
+    return a + rand() % ((b - a) + 1);
+}
--- common/MathHelper.h
+++ common/MathHelper.h
@@ -12,6 +12,10 @@ public:
 
 	template<typename T>
 	static T clamp(const T& x, const T& a, const T& b);
+
+	static float RandFloat(); // random float in [0, 1)
+	static float RandFloat(float a, float b); // random float in [a, b)
+	static int RandInt(int a, int b); // random int in [a, b]
 };
 
 #endif // MATH_HELPER_H
